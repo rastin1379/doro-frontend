1a44b75bd40005bd6337cd07b6765a1401438f19
Nandini <nandinimehrotra14@gmail.com>
2023-10-30 20:17:35 -0400

    Add input fields to get started page

--- src/components/getstarted/getStartedPage.js
+++ src/components/getstarted/getStartedPage.js
@@ -3,6 +3,7 @@ import girlChattingImg from "../../assets/girlChattingWithBot.svg";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import "../../styles/getStartedPage.css";
+import Discord from "../../assets/icons/discord.svg"
 
 function StartSection() {
   return (
@@ -19,11 +20,25 @@ function StartSection() {
             </p>
           </div>
           <img src={girlChattingImg} alt="Girl chatting with bot" />
+          <div className="input-section">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+            <button className="register-button">REGISTER</button>
+            <span className="google-signin">OR CONTINUE WITH GOOGLE</span>
+            <button className="register-button">GOOGLE</button>
+            <span className="join-discord">
+              Join us on Discord{" "}
+              <img src={Discord} alt="Discord" className="discord-icon" />
+            </span>
+          </div>
         </div>
       </div>
       <Footer />
     </div>
   );
 }
+
 
 export default StartSection;
