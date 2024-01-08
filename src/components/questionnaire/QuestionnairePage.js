584ec2ebf57b3aa903f5d80db443b06f8667b91d
Nandini <nandinimehrotra14@gmail.com>
2024-01-08 04:00:19 +0400

    integrating questions endpoint

--- src/components/questionnaire/QuestionnairePage.js
+++ src/components/questionnaire/QuestionnairePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import axios from "axios";
@@ -8,13 +8,21 @@ import ReactMarkdown from "react-markdown";
 import "../../styles/QuestionnairePage.css";
 
 const fetchQuestionnaireData = async (id) => {
+  console.log("FIRST")
   const { data } = await axios.get(`/ptests/${id}/info`);
+  return data;
+};
+
+const fetchQuestions = async (id) => {
+  console.log("SECOND");
+  const { data } = await axios.get(`/ptests/${id}/questions`);
   return data;
 };
 
 const QuestionnairePage = () => {
   const { title, id } = useParams();
   const [answers, setAnswers] = useState({});
+  const [questions, setQuestions] = useState(null);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -32,6 +40,21 @@ const QuestionnairePage = () => {
     refetchOnWindowFocus: false,
     refetchOnMount: false,
   });
+
+  const fetchAdditionalInfo = useCallback(async () => {
+    if (questionnaire) {
+      try {
+        const questions = await fetchQuestions(id);
+        setQuestions(questions);
+      } catch (error) {
+        console.error("Error fetching additional data:", error);
+      }
+    }
+  }, [questionnaire, id]);
+
+  useEffect(() => {
+    fetchAdditionalInfo();
+  }, [fetchAdditionalInfo]);
 
   // Handle loading state
   if (isLoading) {
@@ -100,113 +123,10 @@ const QuestionnairePage = () => {
     </p>
   );
 
-  const questionData = [
-    {
-      id: 1,
-      text: "1. I prefer to do things with others rather than on my own.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 2,
-      text: "2. I prefer to do things the same way over and over again.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 3,
-      text: "3. If I try to imagine something, I find it very easy to create a picture in my mind.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 4,
-      text: "4. I frequently get so strongly absorbed in one thing that I lose sight of other things.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 5,
-      text: "5. I often notice small sounds when others do not.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 6,
-      text: "6. I usually notice car number plates or similar strings of information.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 7,
-      text: "7. Other people frequently tell me that what I've said is impolite, even though I think it is polite.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 8,
-      text: "8. When I'm reading a story, I can easily imagine what the characters might look like.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 9,
-      text: "9. I am fascinated by dates.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-    {
-      id: 10,
-      text: "10. In a social group, I can easily keep track of several different people's conversations.",
-      options: [
-        "Definitely Agree",
-        "Slightly Agree",
-        "Slightly Disagree",
-        "Definitely Disagree",
-      ],
-    },
-  ];
-
-  const questionsContent = questionData?.map((question) => (
+  const questionsContent = questions?.map((question) => (
     <div className="question" key={question.id}>
-      <p>{question.text}</p>
-      {question.options.map((option, index) => (
+      <p>{question.question_text}</p>
+      {question.answer_labels.map((option, index) => (
         <label key={index}>
           <input
             type="radio"
